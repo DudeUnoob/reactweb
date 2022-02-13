6570b00cddbbc8e486b4f8d240e9525ad0155dc7
DudeUnoob <44341783+DudeUnoob@users.noreply.github.com>
2022-02-12 20:57:13 -0600

    react portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import logo from 'file:///C:/Users/tokri/Downloads/Dom%20K%20(1).png';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Dom K's Portfolio</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App'>
+        <button type="button" onClick={()=> window.open("https://pogbotter.herokuapp.com", "_blank")}>Pog Bot</button>
+      </div>
+        
       </header>
-    </div>
-  );
+      
+    </div> 
+    
+    
+  ); 
 }
+
 
 export default App;
